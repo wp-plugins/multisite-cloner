685c9af060f9cc5842f9ceee31aa76c991b9ef2f
ManuelRazzari <ManuelRazzari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-30 21:09:06 +0000

    Initial commit, at version 0.1.4.
    git-svn-id: https://plugins.svn.wordpress.org/multisite-cloner/trunk@924018 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ multisite-cloner.php
@@ -0,0 +1,425 @@
+<?php
+
+/**
+ * Plugin Name: Multisite Cloner
+ * Plugin URI: https://wordpress.org/plugins/multisite-cloner
+ * Description: When creating a new blog on WordPress Multisite, copies all the posts, settings and files, from a selected blog into the new one.
+ * Version: 0.1.4
+ * Author: Manuel Razzari & Patricio Tarantino
+ * Author URI: http://tipit.net
+ * License: License: GPL2+
+**/
+
+/*  Copyright 2014 Tipit.net  (email: manuel@tipit.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+Dejaré mi tierra por tí,
+dejaré mis campos y me iré,
+lejos de aquí.
+
+Cruzaré llorando el jardín,
+y con tus recuerdos partiré,
+lejos de aquí.
+*/
+
+!defined( 'ABSPATH' ) AND exit("Oops!");
+
+add_action('plugins_loaded', array( MultiSiteCloner::get_instance(), 'plugin_setup' ));
+
+class MultiSiteCloner {
+
+    protected static $instance = NULL;
+    
+    function __construct() {
+        register_activation_hook( __FILE__, array( &$this, 'install_multisite_cloner' ) );
+        add_action( 'init', array( &$this, 'init_multisite_cloner' ) );
+    }
+
+    private function get_main_blog_id() {
+        global $current_site;
+        global $wpdb;
+        return $wpdb->get_var ( $wpdb->prepare ( "SELECT `blog_id` FROM `$wpdb->blogs` WHERE `domain` = '%s' AND `path` = '%s' ORDER BY `blog_id` ASC LIMIT 1", $current_site->domain, $current_site->path ) );
+    }
+
+    private function get_first_blog_id() {
+        global $wpdb;
+        return $wpdb->get_var ( "SELECT `blog_id` FROM `$wpdb->blogs` ORDER BY `blog_id` ASC LIMIT 1,1" );
+    }
+
+    
+    public static function get_instance() {
+        NULL === self::$instance and self::$instance = new self;
+        return self::$instance;
+    }
+  
+    function install_multisite_cloner() {
+        if( !is_multisite() )
+            wp_die(
+               'The whole point of this plugin is to clone blogs within a Multisite network. It can\'t be installed on a single site',
+               'Error',
+               array(
+                   'response' => 500,
+                   'back_link' => true
+               )
+           );
+    }
+    public function plugin_setup() {     
+        add_action( 'network_admin_menu', array($this, 'wp_mu_clone_page_link') );
+        add_action( 'wpmu_new_blog', array($this, 'set_new_blog'), 1, 1);
+        add_action( 'admin_footer', array($this, 'clone_input_admin') );
+        add_filter( 'manage_sites_action_links', array($this, 'add_clone_link'), null, 2 );
+        $plugin = plugin_basename(__FILE__);
+        add_filter( "network_admin_plugin_action_links_$plugin", array(&$this, 'cloner_settings_link'), 4, 4 );
+    }
+  
+    function init_multisite_cloner() {
+        add_option('wpmuclone_default_blog', $this->get_first_blog_id() );
+        register_setting( 'default', 'wpmuclone_default_blog' ); 
+    }
+
+    function cloner_settings_link($links, $plugin_file, $plugin_data, $context) {
+      $settings_link = sprintf( '<a href="settings.php?page=wp_mu_clone_settings">%s</a>', __('Settings'));
+      array_unshift($links, $settings_link); 
+      return $links; 
+    }
+
+    function wp_mu_clone_settings() {
+        if ( !empty( $_POST[ 'action' ] ) ) {
+            update_option('wpmuclone_default_blog', $_POST['wpmuclone_default_blog']);
+        }
+
+        ?>
+        <div class="wrap">
+            <style>
+                #wpbody {
+                    background: url(<?php echo plugin_dir_url( __FILE__) ?>/flying-dolly.png) 96% 5% no-repeat;
+                    background-size: 200px;
+                }
+                @media screen and (max-width: 600px) {
+                    #wpbody {
+                        background-image: none;
+                    }
+                }
+            </style>
+            <h2>Multisite Cloner Settings</h2>
+            <form method="post">
+                <?php settings_fields( 'default' ); ?>
+                <h3>Select a default blog to be cloned.</h3>
+                <p>All the data from this blog will be copied into new blogs.</p>
+                <p>This includes settings, posts and other content, theme options, and uploaded files.</p>
+                <table class="form-table">
+                    <tr valign="top">
+                        <th scope="row"><label for="wpmuclone_default_blog">Default Blog</label></th>
+                        <td>
+                        <select name="wpmuclone_default_blog" id="wpmuclone_default_blog">
+                        <?php
+                        $blog_list = wp_get_sites(array('limit' => 0 ));
+                        $main_blog_id = $this->get_main_blog_id();
+                        foreach ($blog_list as $blog) { 
+                            if($blog['blog_id'] != $main_blog_id ):
+                            ?>
+                            <option value="<?php echo $blog['blog_id'];?>" <?php if (get_option('wpmuclone_default_blog') == $blog['blog_id'] ): ?> selected <?php endif; ?>><?php echo get_blog_details( $blog['blog_id'], 'blogname' )->blogname ; ?> ( <?php echo $blog['domain']; ?>)</option>
+                            <?php
+                            endif;
+                        }
+                        ?>                   
+                        </select>
+                        </td>
+                    </tr>
+                </table>
+                <?php submit_button(); ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    function wp_mu_clone_page_link() {
+        add_submenu_page('settings.php', 'Multisite Cloner', 'Multisite Cloner', 'manage_options', 'wp_mu_clone_settings', array($this, 'wp_mu_clone_settings') );
+    }
+
+    function set_new_blog( $blog_id=false ) {
+
+        global $wpdb;
+
+        if ( isset($_POST['wpmuclone_default_blog']) ) {
+            $id_default_blog = intval($_POST['wpmuclone_default_blog']); 
+        } else {
+            $id_default_blog = get_option('wpmuclone_default_blog'); 
+        }
+        
+
+        $old_url = 'http://' . get_blog_details($id_default_blog)->domain;
+        
+        switch_to_blog( $blog_id );
+
+        $new_url = get_site_url();
+        $new_name = get_bloginfo('title','raw');
+        $admin_email = get_bloginfo('admin_email','raw');
+
+        $prefix = $wpdb->base_prefix;
+        $prefix_escaped = str_replace('_','\_',$prefix);
+
+        // List all tables for the default blog,
+        $tables_q = $wpdb->get_results("SHOW TABLES LIKE '" . $prefix_escaped . $id_default_blog . "\_%'");
+
+        foreach($tables_q as $table){
+            $in_array = get_object_vars($table);
+            $old_table_name = current($in_array);
+            $tables[] = str_replace($prefix . $id_default_blog . '_', '', $old_table_name);
+            unset($in_array);
+        }
+
+        // Replace tables from the new blog with the ones from the default blog
+        foreach($tables as $table){
+            $new_table = $prefix . $blog_id . '_' . $table;
+            $old_table = $prefix . $id_default_blog . '_' . $table;
+
+            unset($queries);
+            $queries = array();
+
+            $queries[] = "DROP TABLE IF EXISTS " . $new_table ;
+            $queries[] = "CREATE TABLE " . $new_table . " LIKE " . $old_table;
+            $queries[] = "INSERT INTO " . $new_table . " SELECT * FROM " . $old_table;
+
+            foreach($queries as $query){
+                $wpdb->query($query);
+            }
+
+            $new_tables[] = $new_table;
+        }
+
+        $old_uploads = 'wp-content/uploads/sites/' . $id_default_blog;
+        $new_uploads = 'wp-content/uploads/sites/' . $blog_id;
+
+        // Replace URLs and paths in the DB
+        
+        cloner_db_replacer( array($old_url,$old_uploads), array($new_url,$new_uploads), $new_tables);
+
+        // Update Title
+        update_option('blogname',$new_name);
+
+        // Update Email
+        update_option('admin_email',$admin_email);
+
+        // Copy Files
+        $new_uploads = '../../' . $new_uploads;
+        $old_uploads = '../../' . $old_uploads;
+
+        cloner_recurse_copy($old_uploads, $new_uploads);
+
+        // User Roles
+        $user_roles_sql = "UPDATE wp_" . $blog_id . "_options SET option_name = 'wp_" . $blog_id . "_user_roles' WHERE option_name = 'wp_" . $id_default_blog . "_user_roles';";
+        $wpdb->query($user_roles_sql);
+
+        // Restores main blog
+        switch_to_blog( get_option('wpmuclone_default_blog') );
+    }
+
+    function clone_input_admin() {
+        if( 'site-new-network' != get_current_screen()->base ){
+            return;
+        }
+
+        $dropdown = '<input name="wpmuclone_default_blog" id="wpmuclone_default_blog" type="hidden" value="';
+
+        if ( isset($_GET['clone_from']) ) {
+            $dropdown .= intval($_GET['clone_from']);
+        } else {
+            $dropdown .= get_option('wpmuclone_default_blog') ;
+        }
+
+        $dropdown .= '">';
+
+echo <<<HTML
+    <script type="text/javascript">
+    jQuery(document).ready( function($) {
+        $('$dropdown').appendTo('.form-table');
+    });
+    </script>
+HTML;
+
+    }
+
+    function add_clone_link( $actions, $blog_id ) {     
+        $actions['clone'] = '<a href="'. network_admin_url( 'site-new.php' ).'?clone_from=' . $blog_id . '">Clone</a>';     
+        return $actions;
+    }
+
+
+  
+} // end class
+new MultiSiteCloner();
+
+
+
+/* 
+SEARCH AND REPLACE for WP DBs, taking into account serialized arrays commonly used by plugin options.
+Adapted from the excellent "Search Replace DB" tool by Robert O'Rourke and David Coveney.
+https://github.com/interconnectit/Search-Replace-DB/
+*/
+function cloner_recursive_unserialize_replace( $from = '', $to = '', $data = '', $serialised = false ) {
+
+    // some unseriliased data cannot be re-serialised eg. SimpleXMLElements
+    try {
+
+        if ( is_string( $data ) && ( $unserialized = @unserialize( $data ) ) !== false ) {
+            $data = cloner_recursive_unserialize_replace( $from, $to, $unserialized, true );
+        }
+
+        elseif ( is_array( $data ) ) {
+            $_tmp = array( );
+            foreach ( $data as $key => $value ) {
+                $_tmp[ $key ] = cloner_recursive_unserialize_replace( $from, $to, $value, false );
+            }
+
+            $data = $_tmp;
+            unset( $_tmp );
+        }
+
+        // Submitted by Tina Matter
+        elseif ( is_object( $data ) ) {
+            $dataClass = get_class( $data );
+            $_tmp = new $dataClass( );
+            foreach ( $data as $key => $value ) {
+                $_tmp->$key = cloner_recursive_unserialize_replace( $from, $to, $value, false );
+            }
+
+            $data = $_tmp;
+            unset( $_tmp );
+        }
+
+        else {
+            if ( is_string( $data ) )
+                $data = str_replace( $from, $to, $data );
+        }
+
+        if ( $serialised )
+            return serialize( $data );
+
+    } catch( Exception $error ) {
+
+    }
+
+    return $data;
+}
+
+
+function cloner_db_replacer( $search = '', $replace = '', $tables = array( ) ) {
+
+    global $wpdb;
+
+    $guid = 1;
+    $exclude_cols = array();
+
+    if ( is_array( $tables ) && ! empty( $tables ) ) {
+        foreach( $tables as $table ) {
+
+            $columns = array( );
+
+            // Get a list of columns in this table
+            $fields = $wpdb->query( 'DESCRIBE ' . $table );
+            if ( ! $fields ) {
+                continue;
+            }
+            
+            $columns_gr = $wpdb->get_results( 'DESCRIBE ' . $table );
+
+            foreach($columns_gr as $column){
+                $columns[ $column->Field ] = $column->Key == 'PRI' ? true : false;
+            }
+
+            // Count the number of rows we have in the table if large we'll split into blocks, This is a mod from Simon Wheatley
+            $row_count = $wpdb->get_var( 'SELECT COUNT(*) FROM ' . $table );          
+            if ( $row_count == 0 )
+                continue;
+
+            $page_size = 50000;
+            $pages = ceil( $row_count / $page_size );
+
+            for( $page = 0; $page < $pages; $page++ ) {
+
+                $current_row = 0;
+                $start = $page * $page_size;
+                $end = $start + $page_size;
+                // Grab the content of the table
+                $data = $wpdb->query( sprintf( 'SELECT * FROM %s LIMIT %d, %d', $table, $start, $end ) );
+
+                $rows_gr = $wpdb->get_results( sprintf( 'SELECT * FROM %s LIMIT %d, %d', $table, $start, $end ) );
+
+                foreach($rows_gr as $row) {
+
+                    $current_row++;
+
+                    $update_sql = array( );
+                    $where_sql = array( );
+                    $upd = false;
+
+                    foreach( $columns as $column => $primary_key ) {
+                        if ( $guid == 1 && in_array( $column, $exclude_cols ) )
+                            continue;
+
+                        $edited_data = $data_to_fix = $row->$column;
+
+                        // Run a search replace on the data that'll respect the serialisation.
+                        $edited_data = cloner_recursive_unserialize_replace( $search, $replace, $data_to_fix );
+
+                        // Something was changed
+                        if ( $edited_data != $data_to_fix ) {
+                            $update_sql[] = $column . ' = "' . ( $edited_data ) . '"';
+                            $upd = true;
+                        }
+
+                        if ( $primary_key )
+                            $where_sql[] = $column . ' = "' . ( $data_to_fix ) . '"';
+                    }
+
+                    if ( $upd && ! empty( $where_sql ) ) {
+                        $sql = 'UPDATE ' . $table . ' SET ' . implode( ', ', $update_sql ) . ' WHERE ' . implode( ' AND ', array_filter( $where_sql ) );
+                        $result = $wpdb->query( $sql );
+   
+                    }
+
+                }
+
+            }
+        }
+
+    }
+
+}
+
+/* RECURSIVELY COPY a directory.
+   By gimmicklessgpt at http://php.net/manual/en/function.copy.php#91010 
+*/
+function cloner_recurse_copy($src, $dst) { 
+    $dir = opendir($src); 
+    
+    mkdir($dst);
+
+    while(false !== ( $file = readdir($dir)) ) { 
+        if (( $file != '.' ) && ( $file != '..' )) { 
+            if ( is_dir($src . '/' . $file) ) { 
+                cloner_recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+            else { 
+                copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+        } 
+    } 
+
+    closedir($dir); 
+}
---
+++ readme.txt
@@ -0,0 +1,84 @@
+=== Multisite Cloner ===
+Contributors: manuelrazzari, pmtarantino
+Tags: multisite, wpmu, clone, copy, copy blog, defaults, new blog, network, default settings
+Requires at least: 3.0
+Tested up to: 3.9.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+When creating a new blog on WordPress Multisite, copies all the posts, settings and files, from a selected blog into the new one.
+
+== Description ==
+
+In WordPress Multisite, new sites get a boring default template, without any custom settings or starter content.
+
+This plugins allows you to select a specific blog on your network, a "master" one, that will be cloned every time a new blog is created.
+
+In this way, new blogs will contain all posts, uploads, theme settings and plugin options from the master blog. 
+
+= How does it work? =
+
+1. It uses MySQL `INSERT INTO ... SELECT` to copy *every* table from the "master" blog into the new one.
+1. Then it does a search and replace on the new blog's tables, to replace the old URL with the new one. 
+This is done in a way that respects serialized arrays, so your plugin's settings will be preserved.
+(We used a heavily trimmed down version of Interconnect/IT's [Search and Replace](https://github.com/interconnectit/Search-Replace-DB) tool for that, so go thank them for this!)
+1. It proceeds to copy all files from the `wp-content/uploads` dir of your master blog into the new one's, so that all assets will work as expected.
+1. Finally it does some house clean-up, updating the new blog's title, admin email, and user roles as needed.
+
+Go check the screenshots, it's really simple and does what it says. 
+Give it a try!
+
+*This plugin was handcrafted with love and ‘yerba mate’ by the team at [Tipit.net](http://www.tipit.net/ "Sustainable Web development since 1996 in Austin, Texas").* 
+
+== Frequently Asked Questions ==
+
+= Can I clone the main site? =
+
+No. The main site in your network (usually the one with ID = 1) contains many DB tables, assets and even sensitive information that shouldn't be replicated to other blogs.
+
+= Can I clone my blog in a single-site WP install? =
+
+No. The whole point of this plugin is to clone blogs within a Multisite network. 
+
+= Really? =
+
+We couldn't come up with more questions. Go ahead and ask us some questions and we'll add the frequent ones here :) 
+
+== Installation ==
+
+1. Upload `multisite-cloner` to the `/wp-content/plugins/` directory
+1. Network-activate the plugin through the 'Plugins' menu in your Network admin.
+1. In your Network admin, go to Settings > Multisite Cloner
+
+You'll probably want to create a "master" blog to clone from, if you don't have one already. 
+
+== Screenshots ==
+
+1. **Select a default site.** It will be cloned when any new site is created.
+
+2. **Clone any site.** From the Sites list, admins can easily clone any site in the Network.
+
+== Changelog ==
+
+= 0.1.4 =
+* First public release.
+* Disabled cloning of the main site in the network. Risky stuff.
+* Added "Dolly The Cloner" graphic by rock-star designer Diana Stilinovic.
+
+= 0.1.3 =
+* Added settings page.
+* Added shortcut to clone any blog in the network.
+* Refactored into a plugin class.
+* Refactored DB replacer to use wpdb functions instead of obsolete mysql_* calls.
+
+= 0.1.2 =
+* Replaced calls to functionality in the underlying OS (mysql, sed) with raw SQL, for greater portability.
+
+= 0.1.1 =
+* Initial version, on a client site.
+
+== Upgrade Notice ==
+
+= 0.1.4 =
+This is the first public release.
